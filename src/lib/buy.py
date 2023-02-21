e1940d0bf713b060d08af5588e6438548541d7b7
bmulhern2 <bmulhern09@gmail.com>
2023-02-21 11:34:39 -0500

    deploy

--- src/lib/buy.py
+++ src/lib/buy.py
@@ -3,6 +3,7 @@ from get_stocks import get_stocks
 from get_buying_power import get_buying_power
 from get_if_market_day import get_if_market_day
 from get_positions import get_positions
+from batch_job import batch_job
 import pandas as pd
 import math
 import warnings
@@ -11,33 +12,35 @@ import warnings
 warnings.filterwarnings("ignore")
 
 def buy():
-    stocks = get_stocks()
-    arr = []
-    if stocks:
-        for stock in stocks:
-            last = stock.iloc[-1]
-            arr.append(stock)
-        positions = get_positions()
-        symbols = list(positions.keys())
-        print(symbols)
-        out = []
-        for symbol in symbols:
-            print(symbol)
-            for stock in arr:
-                if symbol == stock['symbol'].iloc[-1]:
-                    continue
-                else:
-                    out.append(stock)
-        try:    
-            out = sorted(out, key=lambda df: df.iloc[-1]['rsi'], reverse=False)
-            power = get_buying_power()
-            power = math.floor(float(power) / 10)
-            if power > 0:
-                arr = sorted(arr, key=lambda df: df.iloc[-1]['rsi'], reverse=False)
-                qty = math.floor(power / arr[0]['Close'].iloc[-1])
-                buy_stock(arr[0]['symbol'].iloc[-1], qty)
-        except Exception as e:
-            print(e)
+    out = batch_job()
+    if out == True:
+        stocks = get_stocks()
+        arr = []
+        if stocks:
+            for stock in stocks:
+                last = stock.iloc[-1]
+                arr.append(stock)
+            positions = get_positions()
+            symbols = list(positions.keys())
+            print(symbols)
+            out = []
+            for symbol in symbols:
+                print(symbol)
+                for stock in arr:
+                    if symbol == stock['symbol'].iloc[-1]:
+                        continue
+                    else:
+                        out.append(stock)
+            try:    
+                out = sorted(out, key=lambda df: df.iloc[-1]['rsi'], reverse=False)
+                power = get_buying_power()
+                power = math.floor(float(power) / 10)
+                if power > 0:
+                    arr = sorted(arr, key=lambda df: df.iloc[-1]['rsi'], reverse=False)
+                    qty = math.floor(power / arr[0]['Close'].iloc[-1])
+                    buy_stock(arr[0]['symbol'].iloc[-1], qty)
+            except Exception as e:
+                print(e)
 
 day = get_if_market_day()
 if day == True:
