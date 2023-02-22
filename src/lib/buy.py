16583247718cdaa7bfbda5be2a5b50b1d2b4c1e1
bmulhern2 <bmulhern09@gmail.com>
2023-02-22 15:34:37 -0500

    deploy

--- src/lib/buy.py
+++ src/lib/buy.py
@@ -26,33 +26,36 @@ def buy():
             return
         else:
             print("Stocks:")
-            for stock in stocks:
-                print(stock['symbol'].iloc[-1])
-            arr = []
-            if stocks:
+            if len(stocks) == 1:
+                print(stocks[0]['symbol'].iloc[-1])
+            else:
                 for stock in stocks:
-                    last = stock.iloc[-1]
-                    if last['sma50'] > last['sma200']:
-                        arr.append(stock)
-                positions = get_positions()
-                symbols = list(positions.keys())
-                out = []
-                for symbol in symbols:
-                    for stock in arr:
-                        if symbol == stock['symbol'].iloc[-1]:
-                            continue
-                        else:
-                            out.append(stock)
-                try:    
-                    out = sorted(out, key=lambda df: df.iloc[-1]['rsi'], reverse=False)
-                    power = get_buying_power()
-                    power = math.floor(float(power) / fraction)
-                    if power > 0:
-                        arr = sorted(arr, key=lambda df: df.iloc[-1]['rsi'], reverse=False)
-                        qty = math.floor(power / arr[0]['Close'].iloc[-1])
-                        buy_stock(arr[0]['symbol'].iloc[-1], qty)
-                except Exception as e:
-                    print(e)
+                    print(stock['symbol'].iloc[-1])
+                arr = []
+                if stocks:
+                    for stock in stocks:
+                        last = stock.iloc[-1]
+                        if last['sma50'] > last['sma200']:
+                            arr.append(stock)
+                    positions = get_positions()
+                    symbols = list(positions.keys())
+                    out = []
+                    for symbol in symbols:
+                        for stock in arr:
+                            if symbol == stock['symbol'].iloc[-1]:
+                                continue
+                            else:
+                                out.append(stock)
+                    try:    
+                        out = sorted(out, key=lambda df: df.iloc[-1]['rsi'], reverse=False)
+                        power = get_buying_power()
+                        power = math.floor(float(power) / fraction)
+                        if power > 0:
+                            arr = sorted(arr, key=lambda df: df.iloc[-1]['rsi'], reverse=False)
+                            qty = math.floor(power / arr[0]['Close'].iloc[-1])
+                            buy_stock(arr[0]['symbol'].iloc[-1], qty)
+                    except Exception as e:
+                        print(e)
 
 day = get_if_market_day()
 if day == True:
--- src/lib/get_assets.py
+++ src/lib/get_assets.py
@@ -2,18 +2,8 @@ from finviz.screener import Screener
 
 def get_assets():
     try:
-        filters = ['sh_price_u5', 'ta_change_u20', 'sh_relvol_o1.5', 'ta_sma50_sa200'] 
+        filters = ['sh_price_u5', 'ta_change_u10', 'sh_relvol_o1.5'] 
         stock_list = Screener(filters=filters, table='Technical', order='-change')
         return stock_list
     except:
-        try:
-            filters = ['sh_price_u5', 'ta_change_u10', 'sh_relvol_o1.5', 'ta_sma50_sa200'] 
-            stock_list = Screener(filters=filters, table='Technical', order='-change')
-            return stock_list
-        except:
-            try:
-                filters = ['sh_price_u5', 'ta_change_u10', 'sh_relvol_o1.5'] 
-                stock_list = Screener(filters=filters, table='Technical', order='-change')
-                return stock_list
-            except:
-                print("No stocksfound")
+        print("No stocksfound")
