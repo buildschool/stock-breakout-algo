2ca490794833cff93edb360a3701d3848c81277c
Brendan Mulhern <119680907+buildschool@users.noreply.github.com>
2023-02-23 00:08:01 -0500

    Update get_assets.py

--- src/lib/get_assets.py
+++ src/lib/get_assets.py
@@ -2,7 +2,7 @@ from finviz.screener import Screener
 
 def get_assets():
     try:
-        filters = ['please add your own settings from fin is pypi'] 
+        filters = ['please add your own settings from finviz pypi'] 
         stock_list = Screener(filters=filters, table='Technical', order='-change')
         return stock_list
     except:
